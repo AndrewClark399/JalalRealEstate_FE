7636d2b1b6ab8e7a6f27fea4b4ed50958807d6ac
liammcnabola <liam.mcnabola@protonmail.com>
2023-12-05 11:59:54 +0000

    set up create sellers

---
+++ src/components/CreateSellers.jsx
@@ -0,0 +1,95 @@
+import axios from "axios";
+import { useState } from "react";
+
+        
+    function CreateSellers() {
+        const [title, setTitle] = useState("");
+        const [firstName, setFirstName] = useState("");
+        const [surname, setSurname] = useState("");
+        const [tel, setTel] = useState("");
+        const [email, setEmail] = useState("");
+        const [address, setAddress] = useState("");
+        
+    
+        return (<form onSubmit={e => {
+            e.preventDefault();
+            axios.post("http://localhost:5000/sellers", { title, firstName, surname, tel, email, address })
+                .then(response => {
+                    console.log(response);
+                    setTitle("");
+                    setFirstName("");
+                    setSurname("");
+                    setTel("");
+                    setEmail("");
+                    setAddress("");
+                })
+                .catch(err => console.error(err))
+        }}>
+            <label htmlFor="sellersTitle" className="form-label">Title</label>
+            <input
+                id="sellersTitle"
+                name="title"
+                className="form-control"
+                type="text"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                required
+            />
+            <label htmlFor="sellersFirstName" className="form-label">First Name</label>
+            <input
+                id="sellersFirstName"
+                name="firstName"
+                className="form-control"
+                type="text"
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+                required
+            />
+            <label htmlFor="sellersSurname" className="form-label">Surname</label>
+            <input
+                id="sellersSurname"
+                name="Surname"
+                className="form-control"
+                type="text"
+                value={surname}
+                onChange={e => setSurname(e.target.value)}
+                required
+            />
+            <label htmlFor="sellersTel" className="form-label">Tel</label>
+            <input
+                id="sellersTel"
+                name="tel"
+                className="form-control"
+                type="number"
+                value={tel}
+                onChange={e => setTel(e.target.value)}
+                required
+            />
+            <label htmlFor="sellersEmail" className="form-label">E-mail</label>
+            <input
+                id="sellersEmail"
+                name="email"
+                className="form-control"
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+            />
+            <label htmlFor="sellersAddress" className="form-label">Address</label>
+            <input
+                id="sellersAddress"
+                name="address"
+                className="form-control"
+                type="text"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+                required
+            />
+            
+            <div className="mt-2">
+                <button className="btn btn-success" type="submit">Submit</button>
+            </div>
+        </form>);
+    }
+    
+export default CreateSellers; 
--- src/components/Sellers.jsx
+++ src/components/Sellers.jsx
@@ -1,8 +1,12 @@
+import CreateSellers from './CreateSellers';
+
 function Sellers() {
     return ( 
-
+<div>
 <h1>Jalal is the place to sell!</h1>
 
+<CreateSellers/>
+</div>
      );
 }
 
