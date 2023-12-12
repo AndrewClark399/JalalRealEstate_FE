192605d121e294e9c2ca06ad98bda9bbeefc0da9
liammcnabola <liam.mcnabola@protonmail.com>
2023-12-12 15:26:23 +0000

    booking stuff

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,10 +1,12 @@
 import CreateBookings from "./CreateBookings";
+import GetBookings from "./GetBookings";
 
 function Bookings () {
     return ( 
     <>
     <h1>Jalal is the place to make bookings to see all the best properties!</h1> 
     <CreateBookings/>
+    <GetBookings/>
     </>
     );
 
